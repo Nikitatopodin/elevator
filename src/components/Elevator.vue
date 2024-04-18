25ea9bcb7458fae465f1ec260a45b7bd5bd4ae57
Никита Быковский <nikitatop1@gmail.com>
2024-04-18 11:53:56 +0300

    feat: add elevator indicator, target level and direction display, fix: fix elevator animation

--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -5,24 +5,81 @@ const props = defineProps(['elevId']);
 const id = props.elevId;
 const elevatorsStore = useElevatorsStore();
 const store = useGeneralStore();
-const levels = store.levels;
+
+const getDirection = () => {
+  if (!elevatorsStore.elevators[id].targetLevel) {
+    return ''
+  }
+  return elevatorsStore.elevators[id].targetLevel <
+    elevatorsStore.elevators[id].currentLevel && elevatorsStore.elevators[id].targetLevel ? '↓' : '↑'
+}
+
+const getCurrentPos = () => {
+  if (elevatorsStore.elevators[id].currentLevel) {
+    return elevatorsStore.elevators[id].currentLevel;
+  }
+
+  if (!elevatorsStore.elevators[id].targetLevel && !elevatorsStore.elevators[id].prevLevel) {
+    return elevatorsStore.elevators[id].currentLevel;
+  }
+
+  if (!elevatorsStore.elevators[id].targetLevel) {
+    return elevatorsStore.elevators[id].prevLevel;
+  }
+
+  return elevatorsStore.elevators[id].targetLevel;
+}
 
 </script>
 
 <template>
   <div class="elevator" :style="{
-    transform: `translateY(-${(elevatorsStore.elevators[id].targetLevel ? elevatorsStore.elevators[id].targetLevel - 1 : elevatorsStore.elevators[id].currentLevel - 1) * 100}%)`,
-    transition: `all ${elevatorsStore.getStartElevatorPos(id)}s`
+    transform: `translateY(-${(getCurrentPos() - 1) * 100}%)`,
+    transition: `linear ${elevatorsStore.getStartElevatorPos(id)}s`,
+    height: `${100 / store.levels}%`
   }">
+    <div
+      :class="elevatorsStore.elevators[id].isReady ? 'elevator__indicator' : 'elevator__indicator elevator__indicator--blocked'">
+    </div>
+    <span class="elevator__target"> {{ elevatorsStore.elevators[id].targetLevel }}</span>
+    <span class="elevator__direction">{{ getDirection() }}</span>
   </div>
 </template>
 
 <style scoped>
+@keyframes blinker {
+  from {
+    background-color: rgb(230, 230, 230)
+  }
+
+  to {
+    background-color: rgb(255, 54, 47)
+  }
+}
+
 .elevator {
   position: absolute;
 
   width: 100%;
-  height: calc(100%/v-bind(levels));
-  background-color: aqua;
+
+  text-align: center;
+
+  background-color: rgb(230, 230, 230);
+  border: 3px solid rgb(130, 130, 130)
+}
+
+.elevator__indicator {
+  width: 15px;
+  height: 15px;
+  margin: 0 auto;
+
+  background-color: rgb(0, 227, 65);
+}
+
+.elevator__indicator--blocked {
+  animation-name: blinker;
+  animation-iteration-count: infinite;
+  animation-timing-function: cubic-bezier(1.0, 0, 0, 1.0);
+  animation-duration: 0.5s;
 }
 </style>
